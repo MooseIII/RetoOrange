12274b22432b109b4fec99ad3ae52a763e225593
Andres Sanchez <javier.sanchez@linktic.com>
2026-01-26 14:47:04 -0500

    Agregado login test con navegador visible y click sobre icono

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -3,15 +3,17 @@ import { test, expect } from '@playwright/test';
 test('Login exitoso', async ({ page }) => {
   await page.goto('https://gal-back-qa.linktic.com/login');
 
-  // Usuario (XPath)
-  await page.locator('//*[@id="q-app"]/div[2]/main/section/div[1]/form/div[1]/div/label/div/div[1]/div[3]/i').click();
+  // Ingresa tu cuenta
+  
+  await page.locator('//*[@id="q-app"]/div[2]/main/section/div[1]/form/div[1]/div/label/div/div[1]/div[3]/i').fill('Interno');
+  await page.waitForTimeout(1000);
+  await page.getByText('Interno', { exact: true }).click();
+  await page.waitForTimeout(1000);
+  await page.locator('[name="username"]').fill('1013581485');
+  await page.waitForTimeout(1000);
+  await page.locator('[name="password"]').fill('Prueba13.');
+  await page.waitForTimeout(1000);
+  await page.locator('.q-icon.on-right.notranslate.material-icons').click();
+  await page.waitForTimeout(3000);
 
-  // Contraseña (XPath)
- // await page.locator('//input[@type="password"]').fill(process.env.PASSWORD!);
-
-  // Botón Ingresar (XPath)
-  //await page.locator('//button[contains(.,"Ingresar")]').click();
-
-  // Validación post-login
-  //await expect(page).not.toHaveURL(/login/);
 });
